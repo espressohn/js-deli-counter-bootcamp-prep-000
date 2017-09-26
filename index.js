7b95f294f8a155112e77d1b9befa9c34a14887ce
espressohn <espressohn@gmail.com>
2017-09-26 16:27:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeli =[]
+
+function takeANumber(katzDeliLine, name){
+    katzDeliLine.push(name)
+    return (katzDeliLine.length+1)
+}
+
+function nowServing(){
+  
+}
+
+function currentLine(){
+  
+}
